17b36eec62d4a2d555c3c38acadf0aaf6d49b877
Natalia <natalia.kielbicka@o2.pl>
2017-10-15 18:35:05 +0200

    add react material ui to searchInput component

--- src/components/views/SearchInput.jsx
+++ src/components/views/SearchInput.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from "react";
+import RaisedButton from "material-ui/RaisedButton";
+import TextField from "material-ui/TextField";
 
 export default class SearchInput extends Component {
   render() {
@@ -6,14 +8,23 @@ export default class SearchInput extends Component {
     return (
       <div>
         <form>
-          <input
+          {/*<input
             type="search"
             id="search"
             placeholder="Search..."
             value={cityName}
             onChange={handleInputChange}
-            onKeyDown={handleKeyPress}/>
-          <button onClick={handleButtonClick}>Search</button>
+          onKeyDown={handleKeyPress}/>*/}
+
+            <TextField hintText={cityName}
+            floatingLabelText={cityName}
+            id="search"
+            placeholder="Search..."
+            value={cityName}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyPress}
+          />
+          <RaisedButton label="Search" onClick={handleButtonClick} primary={true}/>
         </form>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDom from "react-dom";
 import {BrowserRouter} from "react-router-dom";
 import App from "./components/App";
 import "./main.css";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
 document.addEventListener("DOMContentLoaded", function () {
   ReactDom.render((
     <BrowserRouter>
-      <App/>
+      <MuiThemeProvider>
+        <App/>
+      </MuiThemeProvider>
     </BrowserRouter>
   ), document.querySelector("#root"))
 });
