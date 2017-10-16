de13b967e25f57d0c4d98c824e931916975b6ace
Natalia <natalia.kielbicka@o2.pl>
2017-10-16 10:52:09 +0200

    revert changes in search input

--- src/components/views/SearchInput.jsx
+++ src/components/views/SearchInput.jsx
@@ -4,20 +4,15 @@ export default class SearchInput extends Component {
   render() {
     const {cityName, handleInputChange, handleKeyPress, handleButtonClick} = this.props;
     return (
-      <form className="row">
-        <div className="col-md-8">
-          <input
-            type="search"
-            id="search"
-            placeholder="Search..."
-            value={cityName}
-            onChange={handleInputChange}
-            onKeyDown={handleKeyPress}/>
-        </div>
-        <div className="col-md-4 text-center">
-          <button className="btn btn-default" onClick={handleButtonClick}>Search</button>
-        </div>
-
+      <form>
+        <input
+          type="search"
+          id="search"
+          placeholder="Search..."
+          value={cityName}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyPress}/>
+        <button onClick={handleButtonClick}>Search</button>
       </form>
     )
   }
