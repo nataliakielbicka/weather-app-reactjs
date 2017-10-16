bdb0a6eaf5479e24fc25acf6d60f78e34fb0fcce
Natalia <natalia.kielbicka@o2.pl>
2017-10-16 10:47:57 +0200

    Revert "create material navbar"
    This reverts commit d6208e15fbd3b1bfef62330749982bb09243cb33.

--- src/components/views/Menu.jsx
+++ src/components/views/Menu.jsx
@@ -4,17 +4,16 @@ import {Link} from "react-router-dom";
 export default class Menu extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark indigo">
-                <button className="navbar-toggler"><span className="navbar-toggler-icon"/></button>
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <Link to="/" className="nav-link waves-effect waves-light">Home</Link>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
                     </li>
-                    <li className="nav-item">
-                        <Link to="/my-locations" className="nav-link waves-effect waves-light">My locations</Link>
+                    <li>
+                        <Link to="/my-locations">My locations</Link>
                     </li>
-                    <li className="nav-item active">
-                        <Link to="/my-weather" className="nav-link waves-effect waves-light">My weather</Link>
+                    <li>
+                        <Link to="/my-weather">My weather</Link>
                     </li>
                 </ul>
             </nav>
