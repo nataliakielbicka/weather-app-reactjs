51a3fb8be818df490dc8451bda44122317739788
Natalia <natalia.kielbicka@o2.pl>
2017-10-17 22:58:58 +0200

    repair 'React' must be in scope when using JSX error

--- src/components/views/ShowLocations.jsx
+++ src/components/views/ShowLocations.jsx
@@ -1,8 +1,8 @@
 import React, {Component} from "react";
 import axios from "axios";
 
-import WeatherDescription from "./WeatherDescription";
 import LocationItem from "./LocationItem";
+import WeatherDescription from "./WeatherDescription";
 
 export default class ShowLocations extends Component {
     constructor() {
