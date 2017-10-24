4b1b6d0cd62b9c9fd43707473dcdfa78b5fbbb75
Natalia <natalia.kielbicka@o2.pl>
2017-10-24 23:09:57 +0200

    rewrite App component into stateless functional component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,17 @@
-import React, {Component} from "react";
+import React from "react";
 
 import Menu from "./views/Menu";
 import Title from "./views/Title";
 import Main from "./views/Main";
 import Footer from "./views/Footer";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <Menu/>
-        <div className="container">
-          <Title/>
-          <Main/>
-        </div>
-        <Footer/>
-      </div>
-    )
-  }
-}
+const App = () => <div className="wrapper">
+  <Menu/>
+  <div className="container">
+    <Title/>
+    <Main/>
+  </div>
+  <Footer/>
+</div>
+
+export default App;
