384adbcb6887aa85e69d2b198e4e2f7fdc945867
Natalia <natalia.kielbicka@o2.pl>
2017-10-24 19:32:12 +0200

    rewrite SearchInput into stateless functional component

--- src/components/views/SearchInput.jsx
+++ src/components/views/SearchInput.jsx
@@ -1,19 +1,15 @@
-import React, {Component} from "react";
+import React from "react";
 
-export default class SearchInput extends Component {
-  render() {
-    const {cityName, handleInputChange, handleKeyPress, handleButtonClick} = this.props;
-    return (
-      <form className="form">
-        <input className="form__input"
-          type="search"
-          id="search"
-          placeholder="Search a city..."
-          value={cityName}
-          onChange={handleInputChange}
-          onKeyDown={handleKeyPress}/>
-        <button className="form__btn" onClick={handleButtonClick}>Show the weather</button>
-      </form>
-    )
-  }
-}
+const SearchInput = ({cityName, handleInputChange, handleKeyPress, handleButtonClick}) => <form className="form">
+  <input
+    className="form__input"
+    type="search"
+    id="search"
+    placeholder="Search a city..."
+    value={cityName}
+    onChange={handleInputChange}
+    onKeyDown={handleKeyPress}/>
+  <button className="form__btn" onClick={handleButtonClick}>Show the weather</button>
+</form>
+
+export default SearchInput;
