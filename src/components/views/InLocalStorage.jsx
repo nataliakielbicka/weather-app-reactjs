35b737d406ed8eacf4885ffad08e03d2f628eacb
Natalia <natalia.kielbicka@o2.pl>
2017-10-24 16:06:18 +0200

    create InLocalStorage component to display if city is in my locations

---
+++ src/components/views/InLocalStorage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const InLocalStorage = () => <span className="weather__alert">Added :)</span>
+
+export default InLocalStorage;
--- src/components/views/MyLocations.jsx
+++ src/components/views/MyLocations.jsx
@@ -1,6 +1,14 @@
 import React, {Component} from "react";
 
+import InLocalStorage from "./InLocalStorage";
+
 export default class MyLocations extends Component {
+    constructor() {
+        super();
+        this.state = {
+            addedToLocalStorage: false
+        }
+    }
     addToLocalStorage = (e) => {
         e.preventDefault();
         const locations = JSON.parse(localStorage.getItem("myLocations")) || [];
@@ -9,14 +17,25 @@ export default class MyLocations extends Component {
                 this.props.myNewLocation, ...locations
             ];
             localStorage.setItem("myLocations", JSON.stringify(myLocations));
+            this.setState({addedToLocalStorage: true})
         }
     }
     render() {
+        const {addedToLocalStorage} = this.state;
+        const locations = JSON.parse(localStorage.getItem("myLocations")) || [];
         return (
-            <button className="weather__btn"
-                onClick={this
-                .addToLocalStorage
-                .bind(this)}>Add to my Locations</button>
+            <div className="weather__addto">
+                {locations.includes(this.props.myNewLocation) && addedToLocalStorage === false
+                    ? <span className="weather__info">Already added to My locations</span>
+                    : <button
+                        className="weather__btn"
+                        onClick={this
+                        .addToLocalStorage
+                        .bind(this)}>Add to my Locations</button>}
+                {addedToLocalStorage
+                    ? <InLocalStorage/>
+                    : null}
+            </div>
         )
     }
 }
