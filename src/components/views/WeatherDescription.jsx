ac0c3dd467fe34548c748951517aa1c62745790f
Natalia <natalia.kielbicka@o2.pl>
2017-10-24 19:41:27 +0200

    rewrite WeatherDescription into stateless functional component

--- src/components/views/WeatherDescription.jsx
+++ src/components/views/WeatherDescription.jsx
@@ -1,25 +1,26 @@
-import React, {Component} from "react";
+import React from "react";
 
-export default class WeatherDesc extends Component {
-    render() {
-        const {cityName, description, descriptionMain, temperature, weatherIcon} = this.props;
-        const tempCelsius = Math.round((temperature - 32) / (9 / 5));
-        return (
-            <div className="weather__container">
-                <h2 className="weather__header">{cityName}</h2>
-                <span className="weather__temperature">{tempCelsius}
-                    °C {" | "}</span>
-                <span className="weather__temperature">{Math.round(temperature)}
-                    °F</span>
-                {weatherIcon
-                    ? (<img
-                        className="weather__icon"
-                        src={`http://openweathermap.org/img/w/${weatherIcon}.png`}
-                        alt={`${description}`}/>)
-                    : null}
-                <h4>Sky: {description}</h4>
-                <h5>Description: {descriptionMain}</h5>
-            </div>
-        )
-    }
-}
+const WeatherDesc = ({
+    cityName,
+    description,
+    descriptionMain,
+    temperature,
+    weatherIcon,
+    tempCelsius = Math.round((temperature - 32) / (9 / 5))
+}) => <div className="weather__container">
+    <h2 className="weather__header">{cityName}</h2>
+    <span className="weather__temperature">{tempCelsius}
+        °C {" | "}</span>
+    <span className="weather__temperature">{Math.round(temperature)}
+        °F</span>
+    {weatherIcon
+        ? (<img
+            className="weather__icon"
+            src={`http://openweathermap.org/img/w/${weatherIcon}.png`}
+            alt={`${description}`}/>)
+        : null}
+    <h4>Sky: {description}</h4>
+    <h5>Description: {descriptionMain}</h5>
+</div>
+
+export default WeatherDesc;
