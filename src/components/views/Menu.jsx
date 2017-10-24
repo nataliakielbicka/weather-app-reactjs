e1f9bd1a2615d24604aa65eb932de1129d559f4d
Natalia <natalia.kielbicka@o2.pl>
2017-10-24 19:29:46 +0200

    rewrite Menu into stateless functional component

--- src/components/views/Menu.jsx
+++ src/components/views/Menu.jsx
@@ -1,22 +1,18 @@
-import React, {Component} from "react";
+import React from "react";
 import {Link} from "react-router-dom";
 
-export default class Menu extends Component {
-    render() {
-        return (
-            <nav className="navbar">
-                <ul className="navbar__menu menu">
-                    <li className="menu__item">
-                        <Link to="/" className="menu__link">Home</Link>
-                    </li>
-                    <li className="menu__item">
-                        <Link to="/my-locations" className="menu__link">My locations</Link>
-                    </li>
-                    <li className="menu__item">
-                        <Link to="/my-weather" className="menu__link">My weather</Link>
-                    </li>
-                </ul>
-            </nav>
-        )
-    }
-}
+const Menu = () => <nav className="navbar">
+    <ul className="navbar__menu menu">
+        <li className="menu__item">
+            <Link to="/" className="menu__link">Home</Link>
+        </li>
+        <li className="menu__item">
+            <Link to="/my-locations" className="menu__link">My locations</Link>
+        </li>
+        <li className="menu__item">
+            <Link to="/my-weather" className="menu__link">My weather</Link>
+        </li>
+    </ul>
+</nav>
+
+export default Menu;
