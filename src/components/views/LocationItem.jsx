0ffee02fe30e43a1137ccb97bd244bfd3c07ffe1
Natalia <natalia.kielbicka@o2.pl>
2017-10-24 19:27:04 +0200

    rewrite LocationItem into stateless functional component

--- src/components/views/LocationItem.jsx
+++ src/components/views/LocationItem.jsx
@@ -1,17 +1,12 @@
-import React, {Component} from "react";
+import React from "react";
 
-export default class LocationItem extends Component {
-    render() {
-        const {activeItem, cityName, getCityWeather, removeLocation} = this.props;
-        return (
-            <li className={`locations__item${activeItem}`}>
-                <a href="#city" className="locations__link" onClick={getCityWeather}>{cityName.toUpperCase()}</a>
-                <a
-                    href="#remove"
-                    className="locations__remove"
-                    onClick={removeLocation}
-                    title="remove this location">x</a>
-            </li>
-        )
-    }
-}
+const LocationItem = ({activeItem, cityName, getCityWeather, removeLocation}) => <li className={`locations__item${activeItem}`}>
+    <a href="#city" className="locations__link" onClick={getCityWeather}>{cityName.toUpperCase()}</a>
+    <a
+        href="#remove"
+        className="locations__remove"
+        onClick={removeLocation}
+        title="remove this location">x</a>
+</li>
+
+export default LocationItem;
