9ce10008453ba51064ce697eca6c7292a46908ba
Natalia <natalia.kielbicka@o2.pl>
2017-10-24 19:44:12 +0200

    rewrite Main into stateless functional component

--- src/components/views/Main.jsx
+++ src/components/views/Main.jsx
@@ -1,18 +1,14 @@
-import React, {Component} from "react";
+import React from "react";
 import {Switch, Route} from "react-router-dom";
 
 import FetchData from "./FetchData";
 import ShowLocations from "./ShowLocations";
 import MyWeather from "./MyWeather";
 
-export default class Main extends Component {
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/" component={FetchData}/>
-        <Route path="/my-locations" component={ShowLocations}/>
-        <Route path="/my-weather" component={MyWeather}/>
-      </Switch>
-    )
-  }
-}
+const Main = () => <Switch>
+  <Route exact path="/" component={FetchData}/>
+  <Route path="/my-locations" component={ShowLocations}/>
+  <Route path="/my-weather" component={MyWeather}/>
+</Switch>
+
+export default Main;
